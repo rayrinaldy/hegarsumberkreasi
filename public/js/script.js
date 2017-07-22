4b1ec40645f14cf75a24f4b6958ced704354e5e0
Ray <rayrinaldy@gmail.com>
2017-07-22 17:00:59 +0800

    wip

--- public/js/script.js
+++ public/js/script.js
@@ -33,5 +33,22 @@ $(function() {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 2,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
